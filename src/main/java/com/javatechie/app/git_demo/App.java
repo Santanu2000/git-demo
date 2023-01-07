384227d86afd9bed06d3ab4a1b128a6d342ca8a4
Santanu2000 <51831470+Santanu2000@users.noreply.github.com>
2023-01-07 17:19:07 +0530

    addMethod2() added by Developer2

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -9,6 +9,10 @@ public class App {
 		}
 	}
 	
+	public void addMethod2(){
+		System.out.println("Added by Developer2");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}

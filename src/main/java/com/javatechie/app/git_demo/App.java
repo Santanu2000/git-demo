7ba8423c069ff659191dd07103a7aa03dcbea6c1
Santanu Mudi <santanumudi8@gmail.com>
2023-01-07 16:46:11 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -0,0 +1,13 @@
+package com.javatechie.app.git_demo;
+
+public class App {
+
+	public void addData(String name) {
+		System.out.println("data : "+ name);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
